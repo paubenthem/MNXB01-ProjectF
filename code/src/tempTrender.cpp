c822a7637ae9bdbb0ba64cc5fcd6a72d75f324ff
Paulina Benthem <paubenthem@gmail.com>
2021-11-03 14:20:33 +0100

    Part 3.1

--- code/src/tempTrender.cpp
+++ code/src/tempTrender.cpp
@@ -6,8 +6,91 @@ tempTrender::tempTrender(const std::string& filePath) {
     filePath_m = filePath;
 }
 
-// void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const {} //Make a histogram of the temperature on this day
-// void tempTrender::tempOnDay(int dateToCalculate) const {} //Make a histogram of the temperature on this date
+void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const  //Make a histogram of the temperature on this day
+{
+	Int_t year;
+	Int_t month;
+	Int_t day;
+	Int_t h;
+	Int_t min;
+	Int_t sec;
+	Double_t temp;
+
+	// open input file
+	ifstream file(filePath_m);
+
+	// Create histogram	for the temperature of a given day
+	TH1I* hist = new TH1I("temperature", "Temperature; Temperature [#circC]; Entries", 
+				300, -20, 40);
+
+	// Set the Fill color to a darker red
+	hist-> SetFillColor(kRed+1);
+
+	// loop over data 
+	while (file >> year >> month >> day >> h >> min >> sec >> temp) {
+		if (month == monthToCalculate)  {              
+			if (day == dayToCalculate) {
+				hist -> Fill(temp); // Fill histogram with temperature from that day over all years.
+			}
+		}
+	}
+
+	// Calculate the mean temperatura of the given day
+	double mean = hist->GetMean();	
+	// Calculate the standard deviation of the temperature
+	double stdev = hist->GetRMS();	
+	
+	cout << " The mean temperature of the day " << monthToCalculate << dayToCalculate << " is " << mean << "\n" << endl;
+	cout << " The standard deviation of the temperature is " << stdev << "\n" << endl;
+	
+	// Create a new canvas and draw the histogram
+	TCanvas* can = new TCanvas("can", "The temperature of a given day", 900, 600);
+	
+	hist->Draw();
+}
+
+void tempTrender::tempOnDay(int dateToCalculate) const  //Make a histogram of the temperature on this date
+{
+
+	Int_t year;
+	Int_t month;
+	Int_t day;
+	Int_t h;
+	Int_t min;
+	Int_t sec;
+	Double_t temp;
+
+	// open input file
+	ifstream file(filePath_m);
+
+	// Create histogram	for the temperature of a given day
+	TH1I* hist = new TH1I("temperature", "Temperature; Temperature [#circC]; Entries", 
+				300, -20, 40);
+
+	// Set the Fill color to a darker red
+	hist-> SetFillColor(kRed+1);
+
+	// loop over data 
+	while (file >> year >> month >> day >> h >> min >> sec >> temp) {
+		if (TTimeStamp::GetDayOfYear(day, month, year) == dateToCalculate)  {              
+				hist -> Fill(temp); // Fill histogram with temperature from that day over all years.
+		}
+	}
+
+	// Calculate the mean temperatura of the given day
+	double mean = hist->GetMean();	
+	// Calculate the standard deviation of the temperature
+	double stdev = hist->GetRMS();	
+	
+//	cout << " The mean temperature of the day " << monthToCalculate << dayToCalculate << " is " << mean << "\n" << endl;
+//	cout << " The standard deviation of the temperature is " << stdev << "\n" << endl;
+	
+	// Create a new canvas and draw the histogram
+	TCanvas* can = new TCanvas("can", "The temperature of a given day", 900, 600);
+	
+	hist->Draw();
+
+}
 // void tempTrender::tempPerDay() const {} //Make a histogram of the average temperature of each day of the year
 // void tempTrender::hotCold() const {} //Make a histogram of the hottest and coldest day of the year
 // void tempTrender::tempPerYear(int yearToExtrapolate) const {} //Make a histogram of average temperature per year, then fit and extrapolate to the given year
@@ -46,7 +129,7 @@ void tempTrender::covariance(std::string& file2Path){
         //std::cout << t1.AsString("s") << "\t" << t2.AsString("s") << "\n";
         if (t1 == t2){
             //add point to plot
-            plot->AddPoint(temp1,temp2);
+       //     plot->AddPoint(temp1,temp2);
             //load next point
             if (f1.peek() == EOF || f2.peek() == EOF){
                 break;
