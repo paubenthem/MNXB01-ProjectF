4b42c3ee63d123603fa40fa5e62bdcd9753cdb21
Ender <edmund.lehsten@gmail.com>
2021-11-03 16:03:09 +0100

    [bugfix] fixed issues caused by merge

--- code/include/tempTrender.h
+++ code/include/tempTrender.h
@@ -22,11 +22,9 @@ class tempTrender {
 	void tempOnDay(int monthToCalculate, int dayToCalculate) const; //Make a histogram of the temperature on this day
 	void tempOnDay(int dateToCalculate) const; //Make a histogram of the temperature on this date
 	void tempPerDay() const; //Make a histogram of the average temperature of each day of the year
-	// void hotCold() const; //Make a histogram of the hottest and coldest day of the year
-	// void tempPerYear(int yearToExtrapolate) const; //Make a histogram of average temperature per year, then fit and extrapolate to the given year
 
-  void covariance(std::string& file2Path);
-  void covariance(std::string& file2Path,int minuet_timeLag);
+    void covariance(std::string& file2Path);
+    void covariance(std::string& file2Path,int minuet_timeLag);
 
 
 	private:
