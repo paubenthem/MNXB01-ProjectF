57a506e43d5d60dc2079ea949e6ac68032c0db75
Ender <edmund.lehsten@gmail.com>
2021-11-03 15:00:21 +0100

    [bugfix] now works for old versions of root

--- code/src/tempTrender.cpp
+++ code/src/tempTrender.cpp
@@ -42,11 +42,13 @@ void tempTrender::covariance(std::string& file2Path){
     f2 >> year >> month >> day >> hour >> min >> sec >> temp2;
     TTimeStamp t2(year, month, day, hour, min, sec);
     
+    Int_t idx=0;
+
     while (true){
         //std::cout << t1.AsString("s") << "\t" << t2.AsString("s") << "\n";
         if (t1 == t2){
             //add point to plot
-            plot->AddPoint(temp1,temp2);
+            plot->SetPoint(idx++,temp1,temp2);
             //load next point
             if (f1.peek() == EOF || f2.peek() == EOF){
                 break;
@@ -67,8 +69,18 @@ void tempTrender::covariance(std::string& file2Path){
             t2.Set(year, month, day, hour, min, sec,0,kTRUE,0);
         }
     }
-    plot->GetXaxis()->SetTitle("This city");
-    plot->GetYaxis()->SetTitle("Other city");
+
+    size_t start = filePath_m.find_last_of('/');
+    size_t end = filePath_m.find_last_of('.');
+    std::string c1 = filePath_m.substr(start+1,end-start-1);
+
+    start = file2Path.find_last_of('/');
+    end = file2Path.find_last_of('.');
+    std::string c2 = file2Path.substr(start+1,end-start-1);
+
+    plot->SetName("Covariance");
+    plot->GetXaxis()->SetTitle(c1.c_str());
+    plot->GetYaxis()->SetTitle(c2.c_str());
     plot->SetTitle("Covariance Plot");
     plot->Draw("A*");
 }
