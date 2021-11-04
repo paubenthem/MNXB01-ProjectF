c58dbb90b08f8a1987ea5ccde0a0c5f2f664d437
Ender <edmund.lehsten@gmail.com>
2021-11-04 10:35:42 +0100

    [feature] The preprocess script not validates input

--- preprocess.sh
+++ preprocess.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
-mkdir data_clean
-
-for file in $1/*.csv
-do
-    CITYNAME=$(cat ${file} | sed '2q;d' | cut -d';' -f1)
-    echo ${CITYNAME}
-    cat ${file} | #load in file
-        sed '0,/^Datum;Tid/d' | #remove header and lines before
-        sed 's/;[GVY].*//' | #remove the quality code and anything after 
-        sed 's/[;:]/ /g' | #Change ; and : to ' ' (space) 
-        sed 's/\([0-9]\)-/\1 /g' > "data_clean/${CITYNAME}.csv" #remove the - in date and save to new file
-done
+if  [[ $# -eq 1 && -d "$1" ]] ; then #check that arg is foulder
+    mkdir -p data_clean 
+    for file in $1/*.csv
+    do
+        CITYNAME=$(cat ${file} | sed '2q;d' | cut -d';' -f1)
+        echo ${CITYNAME}
+        cat ${file} | #load in file
+            sed '0,/^Datum;Tid/d' | #remove header and lines before
+            sed 's/;[GVY].*//' | #remove the quality code and anything after 
+            sed 's/[;:]/ /g' | #Change ; and : to ' ' (space) 
+            sed 's/\([0-9]\)-/\1 /g' > "data_clean/${CITYNAME}.csv" #remove the - in date and save to new file
+    done
+else
+    echo "The first argument needs to be a directory containing the raw data as .csv files"
+fi
